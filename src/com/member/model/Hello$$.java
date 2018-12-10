1d937a5950f67d338c43c10e948d1862a4b5b7d3
Java <Java@DESKTOP-TQ00GH9>
2018-12-10 20:49:42 +0800

    test123

--- src/com/member/model/Hello$$.java
+++ src/com/member/model/Hello$$.java
@@ -1,10 +1,10 @@
 package com.member.model;
 
-public class Hello {
+public class Hello$$ {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello$$");
+		System.out.println("$$");
 	}
 
 }
